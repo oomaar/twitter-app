c32e3317a3706556b1e62b5d7931e50f0c0c4d18
Omar <omaar_5@hotmail.com>
2021-04-08 14:50:25 +0200

    protectedRoutes Logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { BrowseScreen, HomeScreen, LoginScreen, SignupScreen } from "./screens";
+import useAuthListener from "./hooks/useAuthListner";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 const App = () => {
+  // const user = {};
+  // const user = null;
+  // const user = { name: 'username' };
+  const { user } = useAuthListener();
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={HomeScreen} />
-        <Route exact path="/login" component={LoginScreen} />
-        <Route exact path="/signup" component={SignupScreen} />
-        <Route exact path="/browse" component={BrowseScreen} />
+        <IsUserRedirect
+          user={user}
+          loggedinpath="/browse"
+          exact
+          path="/">
+          <HomeScreen />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedinpath="/browse"
+          exact
+          path="/login">
+          <LoginScreen />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedinpath="/browse"
+          exact
+          path="/signup">
+          <SignupScreen />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} exact path="/browse">
+          <BrowseScreen />
+        </ProtectedRoute>
       </Switch>
     </Router>
   );
